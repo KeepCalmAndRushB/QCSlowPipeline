77da29257aa72d6803e3ac211334bf7b30cb27bb
Deiml <seck-ncp@gmx.de>
2019-03-14 10:39:13 +0100

    Apadte prepare MQ args

--- Snakefile
+++ Snakefile
@@ -1,23 +1,26 @@
 rule copy_raw_file
     input:
-        MS12
-        # new file detected...
+        'D:/singleRAW/20190108_QX0_AlRe_MA_HeLa_500ng_LC05_1/20190108_QX0_AlRe_MA_HeLa_500ng_LC05_1.raw'
+        'D:/Scripttest/20190108_QX0_AlRe_MA_HeLa_500ng_LC05_1/20190108_QX0_AlRe_MA_HeLa_500ng_LC05_1.raw'
     output:
-        'D:/singleRAW/{newfolder}/{newraw}'
+        'D:/Scripttest/20190108_QX0_AlRe_MA_HeLa_500ng_LC05_1/20190108_QX0_AlRe_MA_HeLa_500ng_LC05_1.raw'
+        'D:/Scripttest/20190108_QX0_AlRe_MA_HeLa_500ng_LC05_1'
     shell:
-        "copy {input[0]} {input[1]}"
+        "mkdir D:/Scripttest/20190108_QX0_AlRe_MA_HeLa_500ng_LC05_1"
+        "copy {input[0]} {output[0]}"
 
 rule prepare_max_quant_analysis
     input:
         "data/fasta/20190110_HomoSapiens_95965entries.fasta"
         'D:/singleRAW/{filename}/{filename}.raw'
         'D:/MaxQuant/mqpar.xml'
+        analysis_directory
     params:
         threads = 2
     output:
         'D:/singleRAW/{filename}/mqpar.xml'
     shell:
-        python scripts/'Prepare Max Quant/preparemaxquant.py {input[1]} {input[2]} {params.threads} {output}
+        python scripts/'Prepare Max Quant/preparemaxquant.py {input[1]} {input[2]} {input3} {input4} {params.threads} {output}
 
 rule run_max_quant_analysis:
     input:
--- scripts/Prepare Max Quant analysis/preparemaxquant.py
+++ scripts/Prepare Max Quant analysis/preparemaxquant.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 
 
+
 def clear_parameter_tree(xml_tree):
     """Returns an xml tree object from MaxQuant's mqpar.xml that has some parts missing."""
     root = xml_tree.getroot()
@@ -38,8 +39,26 @@ def main(rawfilename, template_filename, num_threads, parameterfilename):
     numThreads.text = str(num_threads)
     # ET.dump(numThreads)
 
-    tree.write(parameterfilename)
+    tree.write(analysis_directory + "mqpar.xml")
 
 
 if __name__ == '__main__':
-    main(rawfilename, template_filename, num_threads, parameterfilename)
+
+
+
+
+
+
+import argparse
+parser = argparse.ArgumentParser(description="Prepare MaxQuant Analysis.")
+parser.add_argument('Fasta_complete', help="The Fasta File used for the analysis")
+parser.add_argument('Rawfilepath_complete', help="Raw-File Path in analysis directory")
+parser.add_argument('template_path_complete', help="MQ parameter file complete path")
+parser.add_argument('Analysis_directory', help="The Folder where the MQ analysis will take place")
+parser.add_argument('Threads', help="Number of threads used for analysis (default=2)")
+parser.add_argument('template_output', help="MQ parameter file used for the analysis")
+
+args = parser.parse_args()
+
+main(Fasta=args.Fasta_complete, rawfilepath=args.Rawfilepath_complete, template=args.template_path_complete, analysis_directory=args.Analysis_directory, Threads=args.Threads, output_template=args.template_output' )
+
